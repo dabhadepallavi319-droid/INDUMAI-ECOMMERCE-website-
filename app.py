46afd8e53a112bf311576b325c025dc8ace5ab50
Shrawani Kale <kaleshrawani3999@gmail.com>
2026-04-11 22:23:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1090,5 +1090,5 @@ def blog():
 @app.route('/gallery')
 def gallery():
     return render_template('gallery.html')
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
